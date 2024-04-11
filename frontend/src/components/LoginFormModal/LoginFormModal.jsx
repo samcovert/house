748c7aa9f4ed06e73856881ae156a16d0826f434
samcovert13@gmail.com <samcovert13@gmail.com>
2024-04-11 14:02:06 -0600

    merge

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -24,6 +24,20 @@ function LoginFormModal() {
       });
   };
 
+  const handleDemoLogin = (e) => {
+    e.preventDefault()
+    setCredential('Demo-lition')
+    setPassword('password')
+    dispatch(sessionActions.login({ credential: 'Demo-lition', password: 'password' }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
+  }
+
   return (
     <>
       <h1>Log In</h1>
@@ -50,6 +64,7 @@ function LoginFormModal() {
           <p>{errors.credential}</p>
         )}
         <button type="submit">Log In</button>
+        <button onClick={handleDemoLogin}>Log In as Demo User</button>
       </form>
     </>
   );
